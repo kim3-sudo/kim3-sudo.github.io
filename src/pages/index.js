cb527a0439b969f54733b6d1b1a4e5ad1539fe9b
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2022-05-14 15:54:18 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -205,6 +205,148 @@ class Home extends React.Component {
                         </div>
                     </div>
                 </section>
+                <section id="technologies" className="container-fluid mb-5">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-md-10">
+                                <RevealAnimation animationClass="main-reveal-text">
+                                    <h1>Technologies I Know</h1>
+                                </RevealAnimation>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-md-10">
+                                <RevealAnimation animationClass="main-reveal-text">
+                                    <h2>Data Analysis</h2>
+                                </RevealAnimation>
+                            </div>
+                        </div>
+                        <RevealAnimation animationClass="main-fade-in-animation">
+                            <div className="col-md-7 col-12">
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/Rlogo.png" alt="R" />
+                                        <p>R</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/1200px-Python-logo-notext.svg_-1024x1024.png" alt="Python with Pandas" />
+                                        <p>Python with Pandas</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/1_iDQvKoz7gGHc6YXqvqWWZQ.png" alt="TensorFlow and Keras" />
+                                        <p>TensorFlow and Keras</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/Tableau-Emblem-1024x576.png" alt="Tableau" />
+                                        <p>Tableau</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </RevealAnimation>
+                        <div className="row justify-content-center">
+                            <div className="col-md-10">
+                                <RevealAnimation animationClass="main-reveal-text">
+                                    <h2>Data Analysis</h2>
+                                </RevealAnimation>
+                            </div>
+                        </div>
+                        <RevealAnimation animationClass="main-fade-in-animation">
+                            <div className="col-md-7 col-12">
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/1822px-ISO_C_Logo.svg_-267x300.png" alt="C Plus Plus 14" />
+                                        <p>C++ 14</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/Csharp_Logo-edited.png" alt="C Sharp" />
+                                        <p>C#</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/swift-og-1024x1024.png" alt="Swift" />
+                                        <p>Swift</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/PHP-logo.svg_-1024x553.png" alt="PHP" />
+                                        <p>PHP</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/1200px-Python-logo-notext.svg_-1024x1024.png" alt="Python 3" />
+                                        <p>Python 3</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </RevealAnimation>
+                        <div className="row justify-content-center">
+                            <div className="col-md-10">
+                                <RevealAnimation animationClass="main-reveal-text">
+                                    <h2>Database Systems</h2>
+                                </RevealAnimation>
+                            </div>
+                        </div>
+                        <RevealAnimation animationClass="main-fade-in-animation">
+                            <div className="col-md-7 col-12">
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/52232852-e2c4f780-28bd-11e9-835d-1e3cf3e43888.png" alt="Microsoft SQL Server 2019" />
+                                        <p>Microsoft SQL Server 2019</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/1200px-Oracle_SQL_Developer_logo.svg_-902x1024.png" alt="Oracle PL/SQL" />
+                                        <p>Oracle PL/SQL</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/1200px-MySQL_logo.svg_-1024x710.png" alt="MySQL 5" />
+                                        <p>MySQL 5</p>
+                                    </div>
+                                    <div className="col-sm-4 col-5">
+                                        <StaticImage width={200} height={200} className="main-badge" src="../img/Postgresql_elephant.svg_.png" alt="PostgreSQL" />
+                                        <p>PostgreSQL</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </RevealAnimation>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-md-10">
+                            <div className="d-inline-block">
+                                <RevealAnimation animationClass="main-reveal-text">
+                                    <p className="h5">Operating Systems</p>
+                                </RevealAnimation>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center">
+                        <RevealAnimation animationClass="main-fade-in-animation">
+                            <div className="col-lg-12 col-12">
+                                <div className="row justify-content-center">
+                                    <div className="col-sm-4 col-4">
+                                        <h3>RHEL-Type Linux</h3>
+                                        <ul>
+                                            <li>Red Hat Enterprise Linux</li>
+                                            <li>Fedora Linux</li>
+                                            <li>CentOS Linux</li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-sm-4 col-4">
+                                        <h3>Debian-Type Linux</h3>
+                                        <ul>
+                                            <li>Debian Linux</li>
+                                            <li>Ubuntu Linux</li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-sm-4 col-4">
+                                        <h3>Windows</h3>
+                                        <ul>
+                                            <li>Windows 7-10</li>
+                                            <li>Windows Server 2012</li>
+                                            <li>Windows Server 2016</li>
+                                            <li>Windows Server 2019</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </RevealAnimation>
+                </section>
                 <Footer />
             </div>
         )
